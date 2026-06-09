b3701d7abf52127b848cc4c560f548d2ee105ed6
Juvenal <Juvenalhz95@gmail.com>
2026-06-09 11:27:52 -0400

    useCdn en true para usar el cache

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -5,7 +5,7 @@ import imageUrlBuilder from '@sanity/image-url';
 export const client = createClient({
   projectId: 'jxemj9hs', // El ID de tu proyecto de Sanity
   dataset: 'production',
-  useCdn: false, // true para que cargue a la velocidad de la luz usando el caché
+  useCdn: true, // true para que cargue a la velocidad de la luz usando el caché
   apiVersion: '2026-05-21', // Fecha de hoy para congelar la API
 });
 
